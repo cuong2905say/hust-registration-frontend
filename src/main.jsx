2d7027d974360b6dbe796f46a53b54074c0d3b48
Nguyen Manh Cuong (Cuong) <cuong02n@gmail.com>
2024-07-15 18:56:45 +0700

    fix route

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+    <BrowserRouter>
       <App />
-  </React.StrictMode>
+    </BrowserRouter>
 );
